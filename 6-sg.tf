3eec172e043c87b1727d5ff7d05b22a7d07dc1b8
Mariappan <ganesan.mariappan@logpay.de>
2024-11-01 10:11:22 +0100

    change ingress and egress port range to avoid public access

--- 6-sg.tf
+++ 6-sg.tf
@@ -7,14 +7,14 @@ resource "aws_security_group" "sg-terra" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.4.1.0/24"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.4.1.0/24"]
   }
 
   tags = {
