914e0361ddabf1f092a056fe83468a148dfd7548
Mariappan <ganesan.mariappan@logpay.de>
2024-11-01 11:08:23 +0100

    fix for the lint code review

--- 0-provider.tf
+++ 0-provider.tf
@@ -15,14 +15,3 @@ provider "aws" {
 }
 
 
-resource "aws_flow_log" "log_terra" {
-  traffic_type         = "ALL"
-  vpc_id               = aws_vpc.vpc-terra.id
-
-  # Enable the new meta fields
-  log_format = "$${version} $${vpc-id} $${subnet-id} $${instance-id} $${interface-id} $${account-id} $${type} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${protocol} $${packets} $${bytes} $${start} $${end} $${action} $${tcp-flags}"
-
-  tags = {
-    Name = "log_terra"
-  }
-}
