b17b1efc78ba820c7beb37711144dfc677b2d13c
Mariappan <ganesan.mariappan@logpay.de>
2024-11-01 14:43:46 +0100

    fix vpc aws flow log destination

--- 1-vpc.tf
+++ 1-vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "vpc-terra" {
 }
 
 resource "aws_flow_log" "log_terra" {
-  log_destination = "./"
+  log_destination = "arn:aws:s3:::my-local-lp-store-terra/*"
   log_destination_type = "cloud-watch-logs"
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.vpc-terra.id
