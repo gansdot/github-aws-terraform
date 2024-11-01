565222776c18615b25ecc0ccd6c2e35aa6ad5a23
Mariappan <ganesan.mariappan@logpay.de>
2024-11-01 14:55:35 +0100

    fix ec2 httpendpt enabled

--- 7-ec2.tf
+++ 7-ec2.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "ec2-terra" {
-
   ami                         = "ami-0ea3c35c5c3284d82"
   instance_type               = "t2.micro"
   availability_zone           = "us-east-2a"
   subnet_id                   = aws_subnet.pub-sub-terra.id
   vpc_security_group_ids      = [aws_security_group.sg-terra.id]
   associate_public_ip_address = true
-  
+
   metadata_options {
-    http_tokens = "required"
+    http_endpoint = "enabled"
+    http_tokens   = "required"
   }
   root_block_device {
     encrypted = true
