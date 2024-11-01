e6de969d997287e274797a0d3cf9dd6ffb7fb922
Mariappan <ganesan.mariappan@logpay.de>
2024-11-01 14:50:41 +0100

    fix ec2 subnet  instance region us-east-2a instead of 2

--- 3-subnet.tf
+++ 3-subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "pub-sub-terra" {
   vpc_id            = aws_vpc.vpc-terra.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-2"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "pub-sub-terra"
