1ca076cfbcdb21eb2450c8d4273b2300218c6e90
Mariappan <ganesan.mariappan@logpay.de>
2024-11-01 10:21:10 +0100

    fix for the lint code review

--- 7-ec2.tf
+++ 7-ec2.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "ec2-terra" {
   subnet_id                   = aws_subnet.pub-sub-terra.id
   vpc_security_group_ids      = aws_security_group.sg-terra.id
   associate_public_ip_address = true
+  metadata_options {
+    http_tokens = "required"
+  }
+  root_block_device {
+    encrypted = true
+  }
 
   tags = {
     Name = "ec2-terra"
